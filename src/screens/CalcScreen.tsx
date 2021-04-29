3e985fd295e48a76b01f6769731924ce14c288f2
Lalo Aguilar <tallitosan@gmail.com>
2021-04-28 22:17:12 -0500

    style file, calcScreen, basic initialization

---
+++ src/screens/CalcScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from '../theme/appTheme';
+
+export const CalcScreen = () => {
+  return (
+    <View>
+      <Text style={styles.text}>calcScreen</Text>
+    </View>
+  );
+};
